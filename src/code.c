7a8222fa7d60013195bafcd83b846aa26e35bf54
Robert Gravina <robert.gravina@gmail.com>
2017-06-14 23:37:30 +1000

    Write macOS asm to return zero to the OS.

--- src/code.c
+++ src/code.c
@@ -1,5 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "code.h"
 #include "node.h"
 
+void write_file();
+
 void cc_generate_code() {
+  write_file();
+}
+
+void write_file() {
+    FILE *out = fopen("out.s", "wb");
+    fprintf(out, ".section __TEXT,__text\n");
+    fprintf(out, "    .globl _main\n\n");
+    fprintf(out, "_main:\n");
+    fprintf(out, "    movl    $0x2000001, %%eax\n");
+    fprintf(out, "    movl    $0, %%ebx\n");
+    fprintf(out, "    syscall\n");
+    fclose(out);
 }
